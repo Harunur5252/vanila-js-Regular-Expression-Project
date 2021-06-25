5e587734b7eba58306abf0cdd2d425aec81d5ccf
Harunur5252 <hroshid695@gmil.com>
2021-06-25 23:31:42 +0600

    Added password generate and copy option/feature. Some modification in index.htm file and main.js file

--- main.js
+++ main.js
@@ -30,6 +30,13 @@ const data = {
         UI.notMatchUserPasswordWithRegex()
     }
   },
+  random_password_generate(max,min)
+  {
+    const passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+    const randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    const randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+    return randPassword;
+  },
 }
 
 const UI={
@@ -40,8 +47,12 @@ const UI={
     const userNameErr = document.querySelector('.userNameErr');
     const userEmailErr = document.querySelector('.userEmailErr');
     const userPasswordErr = document.querySelector('.userPasswordErr');
+    const gnPwInputElm = document.querySelector('.gnPassInput');
+    const gnPwBtnElm = document.querySelector('.gnPwBtn');
+    const copyPwBtnElm = document.querySelector('.copyPwBtn');
     return {
-        userName,email,userPassword,userNameErr,userEmail,userEmailErr,userPasswordErr
+        userName,email,userPassword,userNameErr,userEmail,userEmailErr,userPasswordErr,
+        gnPwInputElm,gnPwBtnElm,copyPwBtnElm
     }
   },
    matchUserNameWithRegex(){
@@ -59,68 +70,67 @@ const UI={
     userNameErr.style.display='block';
     userName.classList.add('boxE');
     userName.classList.remove('boxS');
- },
- removeUserNameErrAndErrClass(){
+  },
+  removeUserNameErrAndErrClass(){
     const {
       userName,userNameErr
     }=this.loadAllSelectors();
     userName.classList.remove('boxE');
     userNameErr.style.display='none';
- },
- matchUserEmailWithRegex(){
+  },
+  matchUserEmailWithRegex(){
     const {
       userEmail,userEmailErr
     }=this.loadAllSelectors();
     userEmailErr.style.display='none';
     userEmail.classList.add('boxS');
     userEmail.classList.remove('boxE');
- },
- notMatchUserEmailWithRegex(){
+  },
+  notMatchUserEmailWithRegex(){
     const {
       userEmail,userEmailErr
     }=this.loadAllSelectors();
     userEmailErr.style.display='block';
     userEmail.classList.add('boxE');
     userEmail.classList.remove('boxS');
- },
- removeUserEmailErrAndErrClass(){
+  },
+  removeUserEmailErrAndErrClass(){
     const {
       userEmail,userEmailErr
     }=this.loadAllSelectors();
     userEmail.classList.remove('boxE');
     userEmailErr.style.display='none';
- },
- matchUserPasswordWithRegex(){
+  },
+  matchUserPasswordWithRegex(){
     const {
       userPassword,userPasswordErr
     }=this.loadAllSelectors();
     userPasswordErr.style.display='none';
     userPassword.classList.add('boxS');
     userPassword.classList.remove('boxE');
- },
- notMatchUserPasswordWithRegex(){
+  },
+  notMatchUserPasswordWithRegex(){
     const {
       userPassword,userPasswordErr
     }=this.loadAllSelectors();
     userPasswordErr.style.display='block';
     userPassword.classList.add('boxE');
     userPassword.classList.remove('boxS');
- },
- removeUserPasswordErrAndErrClass(){
+  },
+  removeUserPasswordErrAndErrClass(){
     const {
       userPassword,userPasswordErr
     }=this.loadAllSelectors();
     userPassword.classList.remove('boxE');
     userPasswordErr.style.display='none';
- },
-
+  },
   allEventListener(){
     const {
-      userName,userEmail,userPassword
+      userName,userEmail,userPassword,gnPwInputElm,gnPwBtnElm,copyPwBtnElm
     }=this.loadAllSelectors();
     userName.addEventListener('keyup',e=>{
         const userNameInput = e.target.value;
-        const userNameRegex = /[a-z]{4,5}\_?\-?[\D]/gi;
+        const userNameRegex = /[a-z\_\-]{4,10}[\D]/gi; // [a-z]{4,5}\_?\-?[\D]
         data.validCheckUserName(userNameInput,userNameRegex);
     })
 
@@ -132,9 +142,17 @@ const UI={
 
     userPassword.addEventListener('keyup',e=>{
       const userPasswordInput = e.target.value;
-      const userPasswordRegex = /[\w-\.#$&^()(\d)?]{4,8}/gi;
+      const userPasswordRegex = /[\w-\.#@!%&()]{8,16}/gi;
       data.validCheckUserPassword(userPasswordInput,userPasswordRegex);
     })
+    gnPwBtnElm.addEventListener('click',(e)=>{
+       e.preventDefault()
+       gnPwInputElm.setAttribute('value',data.random_password_generate(16,8))
+    })
+    copyPwBtnElm.onclick=function(){
+       gnPwInputElm.select()
+       document.execCommand('Copy')
+    }
   },
   init(){
      this.allEventListener()
@@ -146,4 +164,11 @@ UI.init()
 
 
 
+// function random_password_generate(max,min)
+// {
+//     var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+//     var randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
+//     var randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+//     return randPassword;
+// }
 
