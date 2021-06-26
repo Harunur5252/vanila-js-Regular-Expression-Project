f3c5242e52bc2901f2334c53cd4dad390de83034
Harunur5252 <hroshid695@gmil.com>
2021-06-26 17:12:06 +0600

    Added mobile number validation and some modified index.html and main.js file/module

--- main.js
+++ main.js
@@ -10,6 +10,16 @@ const data = {
          UI.notMatchUserNameWithRegex()
       }
   },
+  validCheckUserMobileNo(userMobileNoInput,userMobileNoRegex){
+    if(userMobileNoRegex.test(userMobileNoInput)){
+       UI.matchUserMobileNoWithRegex()
+    }else if(userMobileNoInput===''){
+       UI.removeUserMobileNoErrAndErrClass()
+    }
+    else{
+       UI.notMatchUserMobileNoWithRegex()
+    }
+  },
   validCheckUserEmail(userEmailInput,userEmailRegex){
     if(userEmailRegex.test(userEmailInput)){
         UI.matchUserEmailWithRegex()
@@ -41,106 +51,136 @@ const data = {
 
 const UI={
   loadAllSelectors(){
-    const userName = document.querySelector('#user_name');
-    const userEmail = document.querySelector('#email');
-    const userPassword = document.querySelector('#password');
-    const userNameErr = document.querySelector('.userNameErr');
-    const userEmailErr = document.querySelector('.userEmailErr');
-    const userPasswordErr = document.querySelector('.userPasswordErr');
+    const userNameElm = document.querySelector('#user_name');
+    const userMobileNoElm = document.querySelector('#mobile_no');
+    const userEmailElm = document.querySelector('#email');
+    const userPasswordElm= document.querySelector('#password');
+    const userNameErrElm = document.querySelector('.userNameErr');
+    const userMobileNoErrElm = document.querySelector('.userMobileNoErr');
+    const userEmailErrElm = document.querySelector('.userEmailErr');
+    const userPasswordErrElm = document.querySelector('.userPasswordErr');
     const gnPwInputElm = document.querySelector('.gnPassInput');
     const gnPwBtnElm = document.querySelector('.gnPwBtn');
     const copyPwBtnElm = document.querySelector('.copyPwBtn');
     return {
-        userName,email,userPassword,userNameErr,userEmail,userEmailErr,userPasswordErr,
-        gnPwInputElm,gnPwBtnElm,copyPwBtnElm
+        userNameElm,userPasswordElm,userNameErrElm,userEmailElm,userEmailErrElm,userPasswordErrElm,
+        gnPwInputElm,gnPwBtnElm,copyPwBtnElm,userMobileNoElm,userMobileNoErrElm
     }
   },
-   matchUserNameWithRegex(){
+  matchUserNameWithRegex(){
     const {
-      userName,userNameErr
+      userNameElm,userNameErrElm
     }=this.loadAllSelectors();
-      userNameErr.style.display='none';
-      userName.classList.add('boxS');
-      userName.classList.remove('boxE');
+      userNameErrElm.style.display='none';
+      userNameElm.classList.add('boxS');
+      userNameElm.classList.remove('boxE');
   },
   notMatchUserNameWithRegex(){
     const {
-      userName,userNameErr
+      userNameElm,userNameErrElm
     }=this.loadAllSelectors();
-    userNameErr.style.display='block';
-    userName.classList.add('boxE');
-    userName.classList.remove('boxS');
+    userNameErrElm.style.display='block';
+    userNameElm.classList.add('boxE');
+    userNameElm.classList.remove('boxS');
   },
   removeUserNameErrAndErrClass(){
     const {
-      userName,userNameErr
+      userNameElm,userNameErrElm
     }=this.loadAllSelectors();
-    userName.classList.remove('boxE');
-    userNameErr.style.display='none';
+    userNameElm.classList.remove('boxE');
+    userNameErrElm.style.display='none';
+  },
+  matchUserMobileNoWithRegex(){
+    const {
+      userMobileNoElm,userMobileNoErrElm
+    }=this.loadAllSelectors();
+    userMobileNoErrElm.style.display='none';
+      userMobileNoElm.classList.add('boxS');
+      userMobileNoElm.classList.remove('boxE');
+  },
+  notMatchUserMobileNoWithRegex(){
+    const {
+      userMobileNoElm,userMobileNoErrElm
+    }=this.loadAllSelectors();
+    userMobileNoErrElm.style.display='block';
+    userMobileNoElm.classList.add('boxE');
+    userMobileNoElm.classList.remove('boxS');
+  },
+  removeUserMobileNoErrAndErrClass(){
+    const {
+      userMobileNoElm,userMobileNoErrElm
+    }=this.loadAllSelectors();
+    userMobileNoElm.classList.remove('boxE');
+    userMobileNoErrElm.style.display='none';
   },
   matchUserEmailWithRegex(){
     const {
-      userEmail,userEmailErr
+      userEmailElm,userEmailErrElm
     }=this.loadAllSelectors();
-    userEmailErr.style.display='none';
-    userEmail.classList.add('boxS');
-    userEmail.classList.remove('boxE');
+    userEmailErrElm.style.display='none';
+    userEmailElm.classList.add('boxS');
+    userEmailElm.classList.remove('boxE');
   },
   notMatchUserEmailWithRegex(){
     const {
-      userEmail,userEmailErr
+      userEmailElm,userEmailErrElm
     }=this.loadAllSelectors();
-    userEmailErr.style.display='block';
-    userEmail.classList.add('boxE');
-    userEmail.classList.remove('boxS');
+    userEmailErrElm.style.display='block';
+    userEmailElm.classList.add('boxE');
+    userEmailElm.classList.remove('boxS');
   },
   removeUserEmailErrAndErrClass(){
     const {
-      userEmail,userEmailErr
+      userEmailElm,userEmailErrElm
     }=this.loadAllSelectors();
-    userEmail.classList.remove('boxE');
-    userEmailErr.style.display='none';
+    userEmailElm.classList.remove('boxE');
+    userEmailErrElm.style.display='none';
   },
   matchUserPasswordWithRegex(){
     const {
-      userPassword,userPasswordErr
+      userPasswordElm,userPasswordErrElm
     }=this.loadAllSelectors();
-    userPasswordErr.style.display='none';
-    userPassword.classList.add('boxS');
-    userPassword.classList.remove('boxE');
+    userPasswordErrElm.style.display='none';
+    userPasswordElm.classList.add('boxS');
+    userPasswordElm.classList.remove('boxE');
   },
   notMatchUserPasswordWithRegex(){
     const {
-      userPassword,userPasswordErr
+      userPasswordElm,userPasswordErrElm
     }=this.loadAllSelectors();
-    userPasswordErr.style.display='block';
-    userPassword.classList.add('boxE');
-    userPassword.classList.remove('boxS');
+    userPasswordErrElm.style.display='block';
+    userPasswordElm.classList.add('boxE');
+    userPasswordElm.classList.remove('boxS');
   },
   removeUserPasswordErrAndErrClass(){
     const {
-      userPassword,userPasswordErr
+      userPasswordElm,userPasswordErrElm
     }=this.loadAllSelectors();
-    userPassword.classList.remove('boxE');
-    userPasswordErr.style.display='none';
+    userPasswordElm.classList.remove('boxE');
+    userPasswordErrElm.style.display='none';
   },
   allEventListener(){
     const {
-      userName,userEmail,userPassword,gnPwInputElm,gnPwBtnElm,copyPwBtnElm
+      userNameElm,userEmailElm,userPasswordElm,gnPwInputElm,gnPwBtnElm,copyPwBtnElm,userMobileNoElm
     }=this.loadAllSelectors();
-    userName.addEventListener('keyup',e=>{
+    userNameElm.addEventListener('keyup',e=>{
         const userNameInput = e.target.value;
-        const userNameRegex = /[a-z\_\-]{4,10}[\D]/gi; // [a-z]{4,5}\_?\-?[\D]
+        const userNameRegex = /[a-z\_\-]{4,10}[\D]?/gi; // [a-z]{4,5}\_?\-?[\D]
         data.validCheckUserName(userNameInput,userNameRegex);
     })
+    userMobileNoElm.addEventListener('keyup',e=>{
+        const userMobileNoInput = e.target.value
+        const userMobileNoRegex = /\+?(\(?88\)?)?\d{11}/gi; 
+        data.validCheckUserMobileNo(userMobileNoInput,userMobileNoRegex);
+    })
 
-    userEmail.addEventListener('keyup',e=>{
+    userEmailElm.addEventListener('keyup',e=>{
       const userEmailInput = e.target.value;
       const userEmailRegex = /[a-z_0-9\.-]{2,20}@[a-z0-9.]{2,20}\.[a-z]{3,6}/gi;
       data.validCheckUserEmail(userEmailInput,userEmailRegex);
     })
 
-    userPassword.addEventListener('keyup',e=>{
+    userPasswordElm.addEventListener('keyup',e=>{
       const userPasswordInput = e.target.value;
       const userPasswordRegex = /[\w-\.#@!%&()]{8,16}/gi;
       data.validCheckUserPassword(userPasswordInput,userPasswordRegex);
@@ -163,7 +203,7 @@ const UI={
 UI.init()
 
 
-
+// 
 
 // function random_password_generate(max,min)
 // {
